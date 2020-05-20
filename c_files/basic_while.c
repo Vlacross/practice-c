95aadccb8aa4f157b53c5d6ac86ec80595999ed1
Tim Vlasuk <timvlasuk@gmail.com>
2020-05-20 08:38:30 -0700

    followed along with basic while loop structure.

---
+++ c_files/basic_while.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+
+  printf("Simple while_loop begin:");
+  int i =0;
+  printf("\n\nPrinting numbers using while loop from 0 - 9 \n\n");
+
+  while (i<10)
+  {printf("%d\n", i);
+  i++;
+  }
+
+  printf("Simple while_loop end.");
+  return 0;
+}
